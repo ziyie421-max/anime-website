8a444571b29616b0aca82b1835f0e8232d6b0e7c
ziyie <3392477833@qq.com>
2026-07-08 11:55:58 +0800

    Fix StreamingResponseBody import path

--- backend/src/main/java/com/anime/website/controller/VideoProxyController.java
+++ backend/src/main/java/com/anime/website/controller/VideoProxyController.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.method.annotation.StreamingResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import java.io.IOException;
 import java.io.InputStream;
